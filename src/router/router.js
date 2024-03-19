f39acb87a00356eafee567fd203bfd186c943c6d
DangKhoa <20130295@st.hcmuaf.edu.vn>
2024-03-19 16:55:58 +0700

    address list page

--- src/router/router.js
+++ src/router/router.js
@@ -16,61 +16,67 @@ import MyAccount from "../components/pages/my-account/MyAccount";
 import MyOrders from "../components/pages/my-orders/MyOrders";
 import OrderDetail from "../components/pages/order-detail/OrderDetail";
 import Wishlist from "../components/pages/wishlist/Wishlist";
+import AddressList from "../components/pages/user-address/AddressList";
 
 export const webRouter = createBrowserRouter([{
-    path: '/',
-    element: <App/>,
-    children: [
-        {
-            path: "",
-            element: <Home/>
-        }, {
-            path: "home",
-            element: <Home/>,
-        }, {
-            path: "sign-in",
-            element: <SignIn/>,
-        }, {
-            path: "sign-up",
-            element: <SignUp/>,
-        }, {
-            path: "product-list",
-            element: <ProductList/>,
-        }, {
-            path: "product-detail",
-            element: <Detail/>,
-        }, {
-            path: "blog-list",
-            element: <BlogList/>,
-        }, {
-            path: "blog-detail",
-            element: <BlogDetail/>,
-        }, {
-            path: "contact",
-            element: <Contact/>,
-        }, {
-            path: "cart",
-            element: <Cart/>,
-        }, {
-            path: "checkout",
-            element: <Checkout/>,
-        }, {
-            path: "not-found",
-            element: <NotFound/>,
-        }, {
-            path: "forgot-password",
-            element: <ForgotPassword/>,
-        }, {
-            path: "my-account",
-            element: <MyAccount/>,
-        }, {
-            path: "my-orders",
-            element: <MyOrders/>,
-        }, {
-            path: "order-detail",
-            element: <OrderDetail/>,
-        }, {
-            path: "wishlist",
-            element: <Wishlist/>,
-        }]
-}]);
+        path: '/',
+        element: <App/>,
+        children: [
+            {
+                path: "",
+                element: <Home/>
+            }, {
+                path: "home",
+                element: <Home/>,
+            }, {
+                path: "sign-in",
+                element: <SignIn/>,
+            }, {
+                path: "sign-up",
+                element: <SignUp/>,
+            }, {
+                path: "product-list",
+                element: <ProductList/>,
+            }, {
+                path: "product-detail",
+                element: <Detail/>,
+            }, {
+                path: "blog-list",
+                element: <BlogList/>,
+            }, {
+                path: "blog-detail",
+                element: <BlogDetail/>,
+            }, {
+                path: "contact",
+                element: <Contact/>,
+            }, {
+                path: "cart",
+                element: <Cart/>,
+            }, {
+                path: "checkout",
+                element: <Checkout/>,
+            }, {
+                path: "not-found",
+                element: <NotFound/>,
+            }, {
+                path: "forgot-password",
+                element: <ForgotPassword/>,
+            }, {
+                path: "my-account",
+                element: <MyAccount/>,
+            }, {
+                path: "address-list",
+                element: <AddressList/>,
+            },
+            {
+                path: "my-orders",
+                element: <MyOrders/>,
+            }, {
+                path: "order-detail",
+                element: <OrderDetail/>,
+            }, {
+                path: "wishlist",
+                element: <Wishlist/>,
+            }]
+    }])
+;
