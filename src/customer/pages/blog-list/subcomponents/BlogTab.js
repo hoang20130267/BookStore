aa2aa7a119a5373818d98078a623f19a78c7737c
hoang20130267 <20130267@st.hcmuaf.edu.vn>
2024-04-04 23:50:38 +0700

    them danh sach tin tuc

--- src/customer/pages/blog-list/subcomponents/BlogTab.js
+++ src/customer/pages/blog-list/subcomponents/BlogTab.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import BlogItem from "./BlogItem";
 import Pagination from "../../../components/general/Pagination";
+import {listBlogs} from "../../../../store/apiRequest";
 
 const BlogTab = () => {
+    const [blogs, setBlogs] = useState([]);
+    useEffect(() => {
+        const fetchBlogs = async () => {
+            try {
+                const data = await listBlogs();
+                setBlogs(data);
+            } catch (error) {
+                console.error("Error fetching blogs:", error);
+            }
+        };
+        fetchBlogs();
+    }, []);
     return (
         <>
             <div className="tab-pane fade active show" id="all_cats" role="tabpanel"
                  aria-labelledby="tab-all_cats">
                 <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3">
-                    <BlogItem/>
-                    <BlogItem/>
-                    <BlogItem/>
-                    <BlogItem/>
+                    {blogs.map(blog => (
+                        <BlogItem
+                            key={blog.id}
+                            title={blog.title}
+                            image={blog.image}
+                            content={blog.content}
+                            created_at={blog.created_at}
+                        />
+                    ))}
                 </div>
             </div>
             <Pagination/>
