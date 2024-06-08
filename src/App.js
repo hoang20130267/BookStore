9a4e9b5df6ce7bc7d0f7aa1dc67621459780dded
hoang20130267 <20130267@st.hcmuaf.edu.vn>
2024-06-08 21:31:35 +0700

    change link main css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import 'mdbreact/dist/css/mdb.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
-import "./customer/assets/css/style.css";
 import "./customer/assets/css/popup.css";
 import './App.css';
 import {Route, Routes} from "react-router-dom";
 import Router from "./router/Router";
+import { Helmet } from 'react-helmet';
 
 
 function App() {
     return (
         <div>
+            <Helmet>
+                <link rel="stylesheet" href="https://host-css-9e18e.web.app/style.css" />
+            </Helmet>
             <Routes>
                 <Route path='/*' element={<Router/>}></Route>
             </Routes>
